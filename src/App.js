916c1645e9b89476eaa4e45af29862b04571cbf0
Alexandr Kolodinsky <Photon.74@gmail.com>
2022-11-27 20:11:56 +0300

    Array in list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from "react"
 import { Form as ClassForm } from "./components/classes/Form"
 import { Form as FuncForm } from "./components/func/Form"
 
 export function App(){
+    const[toggle, setToggle] = useState(true)
+    const[arr, setArr] = useState([{name: 'Dida'}, {name: 'Doda'}, {name: 'Duda'}, {name: 'Dada'}])
+
     return (
         <>
             <h1>Hello world!!!</h1>
             <ClassForm></ClassForm>
             <hr />
-            <FuncForm></FuncForm>
+            <button onClick={() => setToggle(!toggle)}>{toggle ? 'Hide' : 'Show'}</button>
+            {toggle && <FuncForm />}
+            <ul>
+                {arr.map((item) => (
+                    <li>{item.name}</li>
+                ))}
+            </ul>
         </>
     )
 }
