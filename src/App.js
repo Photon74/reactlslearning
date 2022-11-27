5071ed6bd93face14cb336fda28506810d71fa58
Alexandr Kolodinsky <Photon.74@gmail.com>
2022-11-27 19:01:26 +0300

    Class component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Form } from "./components/classes/Form"
+
+export function App(){
+    return (
+        <div>
+            <h1>Hello world!!!</h1>
+            <Form></Form>
+        </div>
+    )
+}
+
+// export default App
